97f0f9d7e1691bc79bd4bfc9e7f27a40e567c700
Denys <86122979+Swartalfheim@users.noreply.github.com>
2022-11-01 17:51:18 +0200

    add project

---
+++ Module2HW3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module2HW3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
